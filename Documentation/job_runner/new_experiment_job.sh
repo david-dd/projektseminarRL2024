e2751b46eaf0392837294bc4f663acd74bf5f144
cl-ire <s85609@htw-dresden.de>
2024-11-29 10:24:42 +0100

    uploaded generation files

--- Documentation/job_runner/new_experiment_job.sh
+++ Documentation/job_runner/new_experiment_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 module load release/23.10  Anaconda3 #load the required modules
 source activate /projects/p078/p_htw_promentat/conda_ps
-cd /projects/p078/p_htw_promentat/smt2020_4/projektseminarRL2024
+cd /home/tosc270g/projects/projektseminarRL2024
+chmod g+w /home/tosc270g/projects/projektseminarRL2024/experiments
 python3.9 ./exp_set_gen_with_env.py
 
