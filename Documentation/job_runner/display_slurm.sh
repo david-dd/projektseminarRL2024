60919c9fc3ff9fe134e5a84d8140f288a1ef2a85
cl-ire <s85609@htw-dresden.de>
2024-12-03 15:39:38 +0100

    fix

--- Documentation/job_runner/display_slurm.sh
+++ Documentation/job_runner/display_slurm.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# Get the current directory path using pwd
+current_dir=$(pwd)
+
 # Loop through all files that start with "slurm" in the current directory
 for file in slurm*; do
     # Check if the file exists (in case no files match the pattern)
     if [[ -f "$file" ]]; then
-        echo "Displaying last 10 lines of $file:"
+         
+        echo
+        echo "-----------------------------------------------------"
+        echo "$current_dir/$file"
+        echo "-----------------------------------------------------"
+        echo
         # Use tail to print the last 10 lines of the file
-        tail -n 10 "$file"
-        echo "----------------------------------"
+        tail -n 15 "$file"
+        echo
+        echo
     fi
 done
